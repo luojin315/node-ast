00cf918b002e1a1f05a33510973b08b8a26c6511
wenrouxiansheng <773559646@qq.com>
2021-06-29 00:37:18 +0800

    react 转 ast语法树

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const Koa = require('koa');
 const Router = require('koa-router')
 const fs = require('fs');
-const parser = require('@babel/parser').parse
-const traverse = require('@babel/traverse').default
-const babel = require('@babel/core')
+const parser = require("@babel/parser")
+// const traverse = require('@babel/traverse').default
+// const babel = require('@babel/core')
 
 const router = new Router();
 const app = new Koa();
 
 router.get('/changeData', async (ctx, next) => {
 
-    const data = fs.readFileSync('E:/20201111/Gitlab/offical-pc-website-builder/src/pc/views/danceTraining/hipHop.js', 'utf-8')
+    const data = fs.readFileSync('D:/project/react-cli/offical-pc-website-builder/src/pc/views/danceTraining/hipHop.js', 'utf-8')
     // const data = fs.readFileSync('E:/20201111/Gitlab/offical-mobile-website-builder/src/beijing/pc/common/js/public.js', 'utf-8')
-    const ast = parser(data, {
-        sourceType: "module", 
+    const ast = parser.parse(data, {
+        sourceType: "module",
+        plugins: [
+            // enable jsx and flow syntax
+            "jsx",
+            "flow",
+        ],
     });
     ctx.response.body = ast
 
