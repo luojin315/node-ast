2df1d3db57f3a00cabee3da843f5ac4d3af12d71
wenrouxiansheng <773559646@qq.com>
2021-07-02 01:48:10 +0800

    解决ast查找值

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const Koa = require('koa');
 const Router = require('koa-router')
 const fs = require('fs');
 const parser = require("@babel/parser")
-// const traverse = require('@babel/traverse').default
+const traverse = require('@babel/traverse').default
 // const babel = require('@babel/core')
+const generate = require('@babel/generator').default;
 
 const router = new Router();
 const app = new Koa();
 
-router.get('/changeData', async (ctx, next) => {
+let ast = null;
 
+router.get('/changeData', async (ctx, next) => {
     const data = fs.readFileSync('D:/project/react-cli/offical-pc-website-builder/src/pc/views/danceTraining/hipHop.js', 'utf-8')
-    // const data = fs.readFileSync('E:/20201111/Gitlab/offical-mobile-website-builder/src/beijing/pc/common/js/public.js', 'utf-8')
-    const ast = parser.parse(data, {
+    ast = parser.parse(data, {
         sourceType: "module",
         plugins: [
             // enable jsx and flow syntax
@@ -20,9 +21,45 @@ router.get('/changeData', async (ctx, next) => {
             "flow",
         ],
     });
-    ctx.response.body = ast
+    traverse(ast, {
+        enter(path) {
+            if (path.type === 'ClassDeclaration') {
+                const obj = path.scope.block.body.body[0].value;
+                const cache = []
+                ctx.response.body = JSON.stringify(obj, function (key, value) {
+                    if (typeof value === 'object' && value !== null) {
+                        if (cache.indexOf(value) !== -1) {
+                            // Circular reference found, discard key
+                            return;
+                        }
+                        // Store value in our collection
+                        cache.push(value);
+                    }
+                    return value;
+                })
 
+                console.log(path.scope.block)
+            }
+        },
+    })
+
+    // ctx.response.body = '已设置'
 })
+
+// router.get('/getData', async (ctx, next) => {
+
+
+//     traverse(ast, {
+//         enter(path) {
+//             if(path.type === 'ClassDeclaration'){
+//                 console.log(path.scope.block.body.body)
+//             }
+//         },
+//     })
+
+//     let code = generate(ast).code;
+//     ctx.response.body = code
+// })
 
 app.use(router.routes());
 
